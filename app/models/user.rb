cce0546cf9df8ea697ee838fb21f2f40089a1491
Diego Perez <diegopr178@gmail.com>
2021-02-24 16:21:51 -0300

    UPDATE: test for description and profile image

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, length: {minimum: 3, maximum: 50}
   validates :name, format: {with: /^[a-zA-Z\s]*$/, multiline: true}
+  validates :description, length: {maximum: 300}
+  validates :description, format: {with: /^((?!.*http.*).)*$/, multiline: true}
+  validates :profile_image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: {less_than: 300.kilobytes}
 
   has_one_attached :profile_image
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,6 +35,43 @@ RSpec.describe User, type: :model do
                 subject.name = 'user_123@!'
                 expect(subject).to_not be_valid
             end
+
+        end
+
+        context 'Description' do
+            
+            it 'must have 300 chars maximum' do
+                subject.description = 'a' * 301
+                expect(subject).to_not be_valid
+            end
+
+            it 'cant have links to another sites' do
+                subject.description = 'go to https://www.google.cl please!'
+                expect(subject).to_not be_valid
+            end
+
+        end
+
+        context 'Profile image' do
+            
+            it 'must have an image' do
+                subject.profile_image.purge
+
+                expect(subject).to_not be_valid
+            end
+
+            it 'must be an image' do
+                subject.profile_image.attach(fixture_file_upload('files/profile_image.pdf', 'application/pdf'))
+
+                expect(subject).to validate_content_type_of(:profile_image).allowing('image/png', 'image/jpg', 'image/jpeg')
+            end
+
+            it 'must have a max size of 5mb' do
+                subject.profile_image.attach(fixture_file_upload('files/image_500kb.jpg', 'image/jpg'))
+
+                expect(subject).to validate_size_of(:profile_image).less_than_or_equal_to(300.kilobytes)
+            end
+            
         end
     end
 end
