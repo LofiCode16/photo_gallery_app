50a669ccbe42453401b2fdbc5976b04f66f493ef
Diego Perez <diegopr178@gmail.com>
2021-02-25 13:20:58 -0300

    UPDATE: facebook strategy settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,12 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, 
+    ENV['PHOTO_GALLERY_APP_FACEBOOK_ID'], 
+    ENV['PHOTO_GALLERY_APP_FACEBOOK_SECRET'], 
+    scope: 'email,name,profile_pic',
+    display: 'popup',
+    token_params: {parse: :json}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
