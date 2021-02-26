c7a5930f6d687c1f60a8273f016e735407f8a12f
Diego Perez <diegopr178@gmail.com>
2021-02-26 16:08:37 -0300

    FIX: syntax error in user attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
       if auth.info.email
         user.email = auth.info.email
       else
-        user.mail = "example#{@@mail_number}@mail.com"
+        user.email = "example#{@@mail_number}@mail.com"
       end
 
       user.password = Devise.friendly_token[0,20]
