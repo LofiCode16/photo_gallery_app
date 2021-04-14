c68674f82155a84af0bcf5b441b3a6a43bc36bd9
Diego perez <diegopr178@gmail.com>
2021-04-14 12:31:00 -0400

    ADD: env file

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module PhotoGallery
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    Dotenv::Railtie.load
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
