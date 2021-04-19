fdbf19b777e275db7effbfc7bfa1c7582041e58f
Diego perez <diegopr178@gmail.com>
2021-04-19 13:14:29 -0400

    UPDATE: edit profile without pass for rrss users and update mail message for facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
