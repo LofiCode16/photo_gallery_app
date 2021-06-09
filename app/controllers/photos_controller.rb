856904187455144117a767f53bc3a8f7f7c7a14f
Diego perez <diegopr178@gmail.com>
2021-06-08 21:30:52 -0400

    UPDATE: followers photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,11 @@ class PhotosController < ApplicationController
 
     def index
         @users = User.all.with_attached_profile_image
-        @photos = Photo.random(10)
+
+        if helpers.user_signed_in?
+            @photos = Photo.last_photos_for_me(current_user, 10)
+        else
+            @photos = Photo.random(10)
+        end
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,4 +6,5 @@ class Photo < ApplicationRecord
   validates :image, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'], size: { less_than: 5120.kilobytes }
 
   scope :random, -> (cant) { all.includes(:user, :likes).with_attached_image.shuffle[0...cant] }
+  scope :last_photos_for_me, -> (user, cant) { where(user_id: user.my_follows.pluck(:id).push(user.id)).includes(:likes, :user).order(created_at: :desc).limit(cant) }
 end
